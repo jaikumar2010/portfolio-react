913a1141a84d16afe99c156932abb4002544e67b
Jai Kumar <jaikumarsoft@gmail.com>
2023-05-02 15:22:12 +0530

    Navbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/experience">Experience</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
